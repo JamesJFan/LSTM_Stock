c5daa531ee1989f695114605e5671c89af3137df
James Fan <james@Jamess-MacBook-Pro.local>
2019-12-26 21:08:57 -0800

    initial commit

---
+++ LSTM.py
@@ -0,0 +1,14 @@
+# Make sure that you have all these libraries available to run code
+# successfully
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request
+import json
+import os
+import numpy as np
+import tensorflow as tf  # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
+
